f28f2edb68e523a160fe45e28d2dbd11ea358053
jaminhood <princeobj5@gmail.com>
2023-10-07 01:34:11 -0700

    fix: main component

--- components/main/index.tsx
+++ components/main/index.tsx
@@ -6,7 +6,7 @@ const Main: FC<ChildrenTypes> = (props) => {
  return (
   <Section cls="text-zinc-950">
    <Container>
-    <div className="p-8 md:max-w-2xl -mt-[5vh] md:-mt-[20vh] bg-zinc-50 md:mx-auto">{props.children}</div>
+    <div className="p-8 md:max-w-2xl -mt-[20vh] bg-zinc-50 md:mx-auto">{props.children}</div>
    </Container>
   </Section>
  )
