8b018e63c26e00f4d0bfc91b376f4cc3874a3f5b
jaminhood <princeobj5@gmail.com>
2023-10-07 01:32:41 -0700

    fix: hero component

--- components/hero/components/HeroContent.tsx
+++ components/hero/components/HeroContent.tsx
@@ -4,7 +4,7 @@ import { FC } from "react"
 const HeroContent: FC<HeroContentTypes> = (props) => {
  return (
   <Container>
-   <div className="h-[70vh] flex justify-center items-center">
+   <div className="md:h-[70vh] h-[40vh] flex justify-center items-center">
     <div className="max-w-xl text-center text-zinc-50">
      <h1 className="text-5xl md:text-7xl font-montserrat-regular">{props.title}</h1>
     </div>
