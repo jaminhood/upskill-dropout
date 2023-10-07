24ccd92b9a22e3f8db8e2485369c263d05a9a4d0
jaminhood <princeobj5@gmail.com>
2023-10-07 01:27:48 -0700

    fix: hero component

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -5,7 +5,7 @@ import HeroOverlay from "./components/HeroOverlay"
 
 const Hero: FC<HeroTypes> = (props) => {
  return (
-  <Section cls="md:h-[80vh] py-[80px]">
+  <Section cls="md:h-[80vh] pt-[80px] pb-[25vh] md:p-0">
    <HeroOverlay img={props.img} />
    <HeroContent title={props.title} />
   </Section>
